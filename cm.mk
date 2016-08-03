9ce5665e6c1b400564a83456f205690a4c25c887
kirito96 <aquammie96@gmail.com>
2016-08-03 18:42:59 -0400

    Initial commit

--- cm.mk
+++ cm.mk
@@ -1,6 +1,9 @@
 # Release name
 PRODUCT_RELEASE_NAME := 5017O
 
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
--- device_5017O.mk
+++ device_5017O.mk
@@ -8,15 +8,15 @@ $(call inherit-product-if-exists, vendor/alcatel/5017O/5017O-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/alcatel/5017O/overlay
 
 
+LOCAL_PATH := device/alcatel/5017O/prebuilt
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/alcatel/5017O/kernel
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
